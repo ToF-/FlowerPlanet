5541bc6aea1a84aeabe76e372b53e22c59839aa2
ToF- <cthibaut@octo.com>
2019-10-05 23:49:41 +0200

    second pentagon

--- Flower.hs
+++ Flower.hs
@@ -1,5 +1,5 @@
 
-{-# LANGUAGE NoMonomorphismRestriction #-}
+ {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -8,12 +8,19 @@ import Diagrams.Backend.SVG.CmdLine
 import Diagrams.TwoD.Vector
 
 diagram :: Diagram B
-diagram = pentagon 1
-    `atop`  penta # translateX 1 # rotateAround p  (a@@rad)
+diagram = pentagon 1 # lc black # showOrigin
+    `atop` (atPoints [pts!!0,pts!!1] (repeat (circle 0.05)) # lw none # fc red)
+    `atop` fromOffsets [v] 
+    `atop`  (penta # rotate ((72+72/2)@@deg) # translate v # lc purple # showOrigin)
     where
     penta = pentagon 1
-    p = (trailVertices penta)!!0
-    a = -pi/5
+    pts = trailVertices penta
+    p,q :: (Double,Double)
+    p = unp2 $ pts!!0 
+    q = unp2 $ pts!!1
+    v :: V2 Double
+    v = (r2 p) ^+^ (r2 q)
+    a = direction v
 
 main = mainWith $ diagram 
 
