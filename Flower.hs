987740bb21e8d6546c3356d217c7afd7704797f1
ToF- <cthibaut@octo.com>
2019-10-06 00:09:31 +0200

    numbered pentagons

--- Flower.hs
+++ Flower.hs
@@ -8,19 +8,17 @@ import Diagrams.Backend.SVG.CmdLine
 import Diagrams.TwoD.Vector
 
 diagram :: Diagram B
-diagram = pentagon 1 # lc black # showOrigin
-    `atop` (atPoints [pts!!0,pts!!1] (repeat (circle 0.05)) # lw none # fc red)
-    `atop` fromOffsets [v] 
-    `atop`  (penta # rotate ((72+72/2)@@deg) # translate v # lc purple # showOrigin)
+diagram = pentagon 1 # lc black 
+    `atop`  (penta # rotate ((72+72/2)@@deg) # translate (vs!!0) # lc purple)
+    `atop`  (penta # rotate ((72+72/2)@@deg) # translate (vs!!1) # lc green)
+    `atop`  (penta # rotate ((72+72/2)@@deg) # translate (vs!!2) # lc blue)
+    `atop`  (penta # rotate ((72+72/2)@@deg) # translate (vs!!3) # lc red)
+    `atop`  (penta # rotate ((72+72/2)@@deg) # translate (vs!!4) # lc orange)
     where
-    penta = pentagon 1
-    pts = trailVertices penta
-    p,q :: (Double,Double)
-    p = unp2 $ pts!!0 
-    q = unp2 $ pts!!1
-    v :: V2 Double
-    v = (r2 p) ^+^ (r2 q)
-    a = direction v
+    penta = pentagon 1 `atop` (atPoints (trailVertices (pentagon 1)) (map (\n -> text (show n) # fontSizeL 0.2) [0..4]))
+    pts = trailVertices (pentagon 1)
+    vs :: [V2 Double]
+    vs = zipWith (\p q -> (r2 (unp2 p)) ^+^ (r2 (unp2 q))) pts (tail pts ++ [head pts])
 
 main = mainWith $ diagram 
 
