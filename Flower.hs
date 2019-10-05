6642cd4cc6ec981a2e2da26600ddd4e9dcc7652d
ToF- <cthibaut@octo.com>
2019-10-05 22:50:13 +0200

    two pentagons juxtaposed

--- Flower.hs
+++ Flower.hs
@@ -9,6 +9,11 @@ import Diagrams.TwoD.Vector
 
 diagram :: Diagram B
 diagram = pentagon 1
+    `atop`  penta # translateX 1 # rotateAround p  (a@@rad)
+    where
+    penta = pentagon 1
+    p = (trailVertices penta)!!0
+    a = -pi/5
 
 main = mainWith $ diagram 
 
